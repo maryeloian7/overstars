0dbf2abc0ec7e1caedabd869ead2f9cf151c15e2
Maryia <maritsydik@gmail.com>
2023-06-06 17:56:50 +0200

    work on the slide

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './mainPage/AppHeader'
 import OurProjects from './ourProjects/OurProjects';
+import AboutUs from './aboutUs/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <OurProjects/>
+      <AboutUs/>
     </div>
   );
 }
---
+++ src/components/ourProjects/OurProjects.js
@@ -0,0 +1,19 @@
+import "../ourProjects/OurProjects.css";
+import Slider from "./Slider";
+
+const OurProjects = () => {
+  return (
+    <div className="projects">
+      <div className="ddd">
+        <div className="__container">
+          <div className="title__projects">Our projects</div>
+          <div className="aaa">
+            <Slider/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurProjects;
