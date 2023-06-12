d3987180f83d4d6fd71d06c48a64d03c1fae27f6
Maryia <maritsydik@gmail.com>
2023-06-12 17:21:47 +0200

    feat:made burger menu and adaptive

--- src/components/mainPage/AppHeader.js
+++ src/components/mainPage/AppHeader.js
@@ -1,35 +1,66 @@
 import logo from "../resources/Logologo.svg";
 import "./AppHeader.css";
 
+import { useState } from "react";
+
 const Header = () => {
-    
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
   return (
-    
     <div className="header__container">
-        <header className="header">
-          <div className="app__header __container">
-            <a href="/" className="logo">
-              <img src={logo} alt="logo" />
-            </a>
-            <nav className="app__menu">
-              <a href="/" className="menu__section">
-                Games
-              </a>
-              <a href="/" className="menu__section">
-                Career
-              </a>
-              <a href="/" className="menu__section">
-                About us
-              </a>
-            </nav>
-            <button className="btn__header">Contact</button>
+      <nav className="nav __containerH ">
+        <div className="fix app__header ">
+          <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+          </a>
+          <div className="item">
+            <ul className={active}>
+              <li className="nav__item">
+                <a href="/" className="nav__link ">
+                  Games
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  Career
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  About us
+                </a>
+              </li>
+              <li className="nav__item contact">
+                <a href="/" className="nav__link">
+                  Contact
+                </a>
+              </li>
+            </ul>
           </div>
-        </header>
-        <div className="background">
-          <h1 className="title__header __container">OVERSTARS GAMES</h1>
+          <button className="btn__header">Contact</button>
+
+          <div onClick={navToggle} className={icon}>
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
         </div>
+      </nav>
+      <div className="background">
+        <h1 className="title__header __container">OVERSTARS GAMES</h1>
+      </div>
     </div>
   );
+
 };
 
 export default Header;
